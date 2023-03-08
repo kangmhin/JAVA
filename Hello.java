9d134510410a1bd351ba0954c15ecee7eb6170bc
kangmhin <109192011+kangmhin@users.noreply.github.com>
2023-03-08 11:19:33 +0900

    JAVA 출력문 입력문 SCANNER 클래스
    ctrl+space 활용
    ctrl+ alt + 방향키 아래 바로 아래 복사 붙여넣음

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int grade = 2;
+		System.out.print("Hello Java " + (3+4));
+		System.out.println("나는 2학년 입니다.");
+		System.out.printf("나는 %d학년 입니다.", grade);
+		System.out.println("나는 " + 3+4);
+		System.out.println("나는"+ " 춘잣");
+		System.out.println("나는 \"안녕\"이라고 말했다.");
+	}
+
+}
---
+++ ScanTest1.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Scanner;
+
+public class ScanTest1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("나이를 입력해주세요 : ");
+		
+		int age = sc.nextInt();
+		System.out.printf("내 나이는 %d입니다.", age);
+		
+	}
+
+}
